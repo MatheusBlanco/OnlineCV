47142945d13c0538c2afc50b006b87feffaaa4fb
MatheusBlanco <msallesblanco@gmail.com>
2021-01-28 16:32:57 -0300

    Starting slider

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function MainPage() {
     return(
-        <p>Página principal</p>
+        <div>
+            <h1>Matheus Salles Blanco</h1>
+            <h2>Estudante de engenharia de software</h2>
+            <h3>Sobre mim</h3>
+            <p>Estudante de engenharia de software com paixão em soluções de usabilidade e requisitos de software, frontend e mobile. Aprendo rápido e estou sempre em busca de conhecimento</p>
+        </div>
     )
 }
 
